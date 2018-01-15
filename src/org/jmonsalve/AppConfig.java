def2bc55cfb45518beaeb5bb5dfa258250bfc484
Juan Monsalve <juanmonsalveh@gmail.com>
2018-01-14 21:40:00 -0500

    Haciendo uso de la clase AppConfig para evitar el uso del archivo spring_config.xml Nota: Hay que definir los Beans dentro de la clase

--- src/org/jmonsalve/AppConfig.java
+++ src/org/jmonsalve/AppConfig.java
@@ -1,10 +1,19 @@
 package org.jmonsalve;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//@ComponentScan(basePackages="org.jmonsalve")
+@Configuration
+@ComponentScan(basePackages="org.jmonsalve")
 public class AppConfig {
 
+	//Configurar los beansaca
+	@Bean
+	public Administrador getAdministrador() {
+		Administrador administrador = new Administrador();
+		return administrador;
+	}
+	
+	
 }
--- src/org/jmonsalve/MainApp.java
+++ src/org/jmonsalve/MainApp.java
@@ -1,18 +1,29 @@
 package org.jmonsalve;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
 	
 	public static void main(String[] args) {
+		/*//Haciendo uso del archivo xml de configuracion
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring_config.xml");
 		Administrador administrador = (Administrador) applicationContext.getBean("admin");
 		System.out.println(administrador);
 		administrador.imprimirDireccion();
 
 		((ClassPathXmlApplicationContext)applicationContext).close();
+		*/
+		
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		Administrador administrador = applicationContext.getBean(Administrador.class);
+		System.out.println(administrador);
+		administrador.imprimirDireccion();
+		
+		
+		
 	}
 
 
