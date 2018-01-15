7d7c2c61630d5b17a0886892abd120df81ccf053
Juan Monsalve <juanmonsalveh@gmail.com>
2018-01-14 21:45:22 -0500

    Omitiendo de forma Definitiva el uso del archivo spring_config.xml para evitar etiquetas. Se agrega @Component a Administrador para que el @ComponentScan de AppConfig lo detecte como Bean

--- src/org/jmonsalve/AppConfig.java
+++ src/org/jmonsalve/AppConfig.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages="org.jmonsalve")
 public class AppConfig {
-
+/* Basta con poner @Component en Administrador y el @ComponentScan lo identificara
 	//Configurar los beansaca
 	@Bean
 	public Administrador getAdministrador() {
-		Administrador administrador = new Administrador();
+		Administrador administrador = new Administrador(1,"Juan");
 		return administrador;
 	}
-	
+	*/
 	
 }
